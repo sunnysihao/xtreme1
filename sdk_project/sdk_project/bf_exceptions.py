38768993ca7cb51c9ef7856a90ac403192cf6694
sunny <601619961@qq.com>
2023-01-05 09:42:08 +0800

    base version

--- sdk_project/sdk_project/bf_exceptions.py
+++
@@ -1,5 +0,0 @@
-class BFException(Exception):
-    def __init__(self, code, message=''):
-        super().__init__(f'<{code}> {message}')
-
-
--- sdk_project/setup.py
+++
@@ -1,4 +0,0 @@
-from segments import SegmentsDataset, SegmentsClient
-api_key = '982734896'
-a = SegmentsClient(api_key)
-a.upload_asset()
