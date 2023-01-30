50160ec4f5deb0cd452b8122644c375d27925436
ShirleySprite <583882690@qq.com>
2023-01-30 17:42:50 +0800

    None

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xtreme1",  # 用自己的名替换其中的YOUR_USERNAME_
-    version="0.0.1",  # 包版本号，便于维护版本
+    version="0.0.2",  # 包版本号，便于维护版本
     author="Basic AI",  # 作者，可以写自己的姓名
     author_email="zhangsihao@basicfinder.com",  # 作者联系方式，可写自己的邮箱地址
     description="xtreme1 sdk",  # 包的简述
