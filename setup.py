094adfc21110dfa8db2e188f30efb209c137c4db
ShirleySprite <583882690@qq.com>
2023-01-30 17:12:16 +0800

    None

--- setup.py
+++ setup.py
@@ -18,5 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'pyecharts',
+        'rich'
+        'requests'
+    ],
     python_requires='>=3.9',  # 对python的最低版本要求
 )
